02dce9a6056d77c7ae2536012a01f81265d81f25
Deborah Barnard <deborah@starfallprojects.co.uk>
2022-07-19 14:16:18 +0100

    lowercase creds

--- credentials/NasaPicsApi.credentials.ts
+++ credentials/NasaPicsApi.credentials.ts
@@ -6,7 +6,7 @@ import {
 
 export class NasaPicsApi implements ICredentialType {
 
-	name = 'NasaPicsApi';
+	name = 'nasaPicsApi';
 	displayName = 'NASA Pics API';
 	// Uses the link to this tutorial as an example
 	// Replace with your own docs links when building your own nodes
